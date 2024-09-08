065911a76583921d60cff2b787b5fbcb084fc6d1
Natalia Papina <papina@scamt-itmo.ru>
2024-09-08 21:26:52 +0300

    Column names have been changed

--- preprocessing.py
+++ preprocessing.py
@@ -2,4 +2,11 @@ import pandas as pd
 
 data = pd.read_csv("nanotox_dataset.tsv", sep="\t", header='infer')
 
-print(data.info())
+data = data.rename(columns={'Hsf': 'standard_enthalpy_of_formation',
+                     'Ec': 'conduction_band_energy',
+                     'Ev': 'valence_band_energy',
+                     'MeO': 'Mulliken_electronegativity',
+                     'ratio': 'polarization_ratio',
+                     'e': 'pauling_electronegativity',
+                     'esum': 'summation_of_electronegativity',
+                     'esumbyo': 'ratio_of_esum_to_Noxygen'})
