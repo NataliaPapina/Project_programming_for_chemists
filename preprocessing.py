15b99ee2d85497274c87d7566164bb9422533cff
Natalia Papina <papina@scamt-itmo.ru>
2024-09-10 10:57:11 +0300

    normalization update

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import sklearn
-from sklearn.preprocessing import StandardScaler
 
 data = pd.read_csv("nanotox_dataset.tsv", sep="\t", header='infer')
 
@@ -19,18 +17,17 @@ data_normalized = data.copy()
 
 data_normalized['conduction_band_energy'] = data_normalized['conduction_band_energy'].map(lambda x: np.log(x + 6.7))
 data_normalized['dosage'] = data_normalized['dosage'].map(lambda x: np.log10(x))
-data_normalized['Expotime'] = data_normalized['Expotime'].map(lambda x: np.log2(x))
-data_normalized['surfarea'] = data_normalized['surfarea'].apply(lambda x: (x-data['surfarea'].mean())/data['surfarea'].std())
-data_normalized['standard_enthalpy_of_formation'] = data_normalized['standard_enthalpy_of_formation'].map(lambda x: np.log(x+18.345))
-data_normalized['surfcharge'] = data_normalized['surfcharge'].map(lambda x: np.log(x+42.6))
-data_normalized['valence_band_energy'] = data_normalized['valence_band_energy'].map(lambda x: np.log(x+10.81))
+data_normalized['Expotime'] = data_normalized['Expotime'].map(lambda x: np.log(1+(x-min(data['Expotime']))/(max(data['Expotime'])-min(data['Expotime']))))
+data_normalized['surfcharge'] = data_normalized['surfcharge'].apply(lambda x: (x-data['surfcharge'].mean())/data['surfcharge'].std())
+data_normalized['standard_enthalpy_of_formation'] = data_normalized['standard_enthalpy_of_formation'].map(lambda x: np.log(x+abs(min(data['standard_enthalpy_of_formation']))+1))
+data_normalized['valence_band_energy'] = data_normalized['valence_band_energy'].map(lambda x: np.log(x+abs(min(data['valence_band_energy']))+1))
 
-variables = ['coresize', 'hydrosize', 'Mulliken_electronegativity', 'enthalpy_of_formation_of_cation', 'polarization_ratio', 'pauling_electronegativity', 'summation_of_electronegativity', 'ratio_of_esum_to_Noxygen', 'MW', 'NMetal', 'NOxygen', 'ox']
+variables = ['coresize', 'surfarea', 'hydrosize', 'Mulliken_electronegativity', 'enthalpy_of_formation_of_cation', 'polarization_ratio', 'pauling_electronegativity', 'summation_of_electronegativity', 'ratio_of_esum_to_Noxygen', 'MW', 'NMetal']
 
 for var in variables:
     data_normalized[var] = data_normalized[var].map(lambda x: np.log(x))
 
-independent_variables = data[['coresize', 'hydrosize', 'surfcharge',
+df_independent_variables = data[['coresize', 'hydrosize', 'surfcharge',
                               'surfarea', 'standard_enthalpy_of_formation',
                               'conduction_band_energy', 'valence_band_energy',
                               'Mulliken_electronegativity', 'Expotime',
@@ -40,7 +37,7 @@ independent_variables = data[['coresize', 'hydrosize', 'surfcharge',
                               'ratio_of_esum_to_Noxygen', 'MW',
                               'NMetal', 'NOxygen', 'ox']]
 
-independent_variables_normalized = data_normalized[['coresize', 'hydrosize', 'surfcharge',
+df_independent_variables_normalized = data_normalized[['coresize', 'hydrosize', 'surfcharge',
                                                     'surfarea', 'standard_enthalpy_of_formation',
                                                     'conduction_band_energy', 'valence_band_energy',
                                                     'Mulliken_electronegativity', 'Expotime',
