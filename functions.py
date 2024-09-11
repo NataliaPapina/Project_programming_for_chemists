1416b58d5ceb836865dbe884e2d27c762db5feca
Natalia Papina <papina@scamt-itmo.ru>
2024-09-11 23:30:37 +0300

    functions for normalization

--- functions.py
+++ functions.py
@@ -1,46 +1,27 @@
-import scipy.stats as stats
-from scipy.stats import shapiro
-from scipy.stats import lognorm
-import math
-from scipy.stats import kstest
-from scipy.stats import norm
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+
+def encode(lst_to_encode, lst_res, df, df_new):
+    for i in lst_to_encode:
+        le = LabelEncoder()
+        le.fit(df[i])
+        df_new[i] = le.transform(df[i])
+        lst_res.append(dict(zip(df_new[i], df[i])))
 
 
-def grubbs_test(x): # оценка выбросов
-    n = len(x)
-    mean_x = np.mean(x)
-    sd_x = np.std(x)
-    numerator = max(abs(x-mean_x))
-    g_calculated = numerator/sd_x
-    t_value = stats.t.ppf(1 - 0.05 / (2 * n), n - 2)
-    g_critical = ((n - 1) * np.sqrt(np.square(t_value))) / (np.sqrt(n) * np.sqrt(n - 2 + np.square(t_value)))
-    if g_critical > g_calculated:
-        return False
+def norm_log(df, var):
+    if max(df[var]) >= 200:
+        df[var] = df[var].map(lambda x: np.log10(x))
     else:
-        return True
+        df[var] = df[var].map(lambda x: np.log(x))
 
 
-def norm_distribution_test(data, var):
-    if shapiro(data[var])[1] < 0.05 and kstest(data[var], 'norm')[1] > 0.05:
-        return True
-    else:
-        return False
-
-def norm_log(data, var):
-    if max(data[var])>=1000:
-        data_normalized[var] = data[var].map(lambda x: np.log10(x))
-    elif 100<=max(data[var])<1000:
-        data_normalized[var] = data[var].map(lambda x: np.log2(x))
-    else:
-        data_normalized[var] = data[var].map(lambda x: np.log(x))
-
-
-def normalize(data, variables):
+def normalize(df, variables):
     for var in variables:
-        if min(data[var]) <= 0:
-            data_normalized[var] = data[var].map(lambda x: x + min(data[var]) + 1)
-            norm_log(data, var)
+        if min(df[var]) > -1 and max(df[var]) < 11:
+            continue
+        elif min(df[var]) <= 0:
+            df[var] = df[var].map(lambda x: x + abs(min(df[var])) + 1)
+            norm_log(df, var)
         else:
-            norm_log(data, var)
-
-normalize(data, variables)
+            norm_log(df, var)
