4c9bef1dc53693e8f557dcd28100f69aa706f843
Natalia Papina <papina@scamt-itmo.ru>
2024-09-19 15:22:07 +0300

    structure has been changed

--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,14 @@
 import pandas as pd
-from functions import normalize, encode
+from functions import encode
 import pymatgen.core as pmg
 
-
 data = pd.read_csv("nanotox_dataset.tsv", sep="\t", header='infer')
+
+missing_values = data.isnull().sum()
+print(f"Missing values in the data: {missing_values}")
+
+print(f"Duplicates: {data.duplicated().sum()}")
+data = data.drop_duplicates(keep='first')
 
 data = data.rename(columns={'Hsf': 'standard_enthalpy_of_formation',
                      'Ec': 'conduction_band_energy',
@@ -21,6 +26,7 @@ data['total_electrons'] = data['NPs'].apply(lambda x: pmg.Composition(x).total_e
 data['to_weight'] = data['NPs'].apply(lambda x: pmg.Composition(x).to_weight_dict[pmg.Composition(x).elements[0].name])
 data['metal_atomic_mass'] = data['NPs'].apply(lambda x: pmg.Composition(x).elements[0].atomic_mass)
 data['Z'] = data['NPs'].apply(lambda x: pmg.Composition(x).elements[0].Z)
+data['atomic_radius'] = data['NPs'].apply(lambda x: pmg.Composition(x).elements[0].atomic_radius)
 data['atomic_radius_calculated'] = data['NPs'].apply(lambda x: pmg.Composition(x).elements[0].atomic_radius_calculated)
 data['van_der_waals_radius'] = data['NPs'].apply(lambda x: pmg.Composition(x).elements[0].van_der_waals_radius)
 data['electrical_resistivity'] = data['NPs'].apply(lambda x: pmg.Composition(x).elements[0].electrical_resistivity)
@@ -28,36 +34,17 @@ data['ionic_radii'] = data['NPs'].apply(lambda x: pmg.Composition(x).elements[0]
 data['group'] = data['NPs'].apply(lambda x: pmg.Composition(x).elements[0].group)
 data['ionization_energy'] = data['NPs'].apply(lambda x: pmg.Composition(x).elements[0].ionization_energy)
 data['electron_affinity'] = data['NPs'].apply(lambda x: pmg.Composition(x).elements[0].electron_affinity)
+data['boiling_point'] = data['NPs'].apply(lambda x: pmg.Composition(x).elements[0].boiling_point)
+data['brinell_hardness'] = data['NPs'].apply(lambda x: pmg.Composition(x).elements[0].brinell_hardness)
+data['coefficient_of_linear_thermal_expansion'] = data['NPs'].apply(lambda x: pmg.Composition(x).elements[0].coefficient_of_linear_thermal_expansion)
+data['density_of_solid'] = data['NPs'].apply(lambda x: pmg.Composition(x).elements[0].density_of_solid)
+data['liquid_range'] = data['NPs'].apply(lambda x: pmg.Composition(x).elements[0].liquid_range)
+data['melting_point'] = data['NPs'].apply(lambda x: pmg.Composition(x).elements[0].melting_point)
 
 data_normalized = data.copy()
 
 data_normalized['viability'] = data_normalized['viability'].map(lambda x: x if x >= 0 else 0)
 
-encoded = []
+print(encode(['Cellline', 'Celltype', 'class'], data, data_normalized, encoded=None), sep='\n')
 
-encode(['Cellline', 'Celltype', 'class'], encoded, data, data_normalized)
-
-#print(*encoded, sep='\n')
-
-variables = ['coresize', 'hydrosize', 'surfcharge', 'surfarea', 'standard_enthalpy_of_formation',
-             'conduction_band_energy', 'valence_band_energy', 'Mulliken_electronegativity', 'Cellline',
-             'Celltype', 'Expotime', 'dosage', 'enthalpy_of_formation_of_cation', 'polarization_ratio',
-             'pauling_electronegativity', 'summation_of_electronegativity', 'ratio_of_esum_to_Noxygen', 'MW',
-             'NMetal', 'NOxygen', 'ox', 'viability','average_electroneg', 'num_atoms', 'total_electrons',
-             'to_weight', 'metal_atomic_mass', 'Z', 'atomic_radius_calculated', 'van_der_waals_radius',
-             'electrical_resistivity', 'ionic_radii', 'group', 'ionization_energy', 'electron_affinity']
-
-normalize(data_normalized, variables)
-
-df_independent_variables = pd.DataFrame(data_normalized[['coresize', 'hydrosize', 'surfcharge', 'surfarea',
-                                                         'standard_enthalpy_of_formation', 'conduction_band_energy',
-                                                         'valence_band_energy', 'Mulliken_electronegativity',
-                                                         'Cellline', 'Celltype', 'Expotime', 'dosage',
-                                                         'enthalpy_of_formation_of_cation', 'polarization_ratio',
-                                                         'pauling_electronegativity', 'summation_of_electronegativity',
-                                                         'ratio_of_esum_to_Noxygen', 'MW', 'NMetal', 'NOxygen', 'ox',
-                                                         'average_electroneg', 'num_atoms', 'total_electrons',
-                                                         'to_weight', 'metal_atomic_mass', 'Z',
-                                                         'atomic_radius_calculated', 'van_der_waals_radius',
-                                                         'electrical_resistivity', 'ionic_radii', 'group',
-                                                         'ionization_energy', 'electron_affinity']])
+df_independent_variables = pd.DataFrame(data_normalized.drop(['NPs', 'class', 'viability'], axis=1))
