bd14360e0fd4a881b216a691ae477bc74a59f77c
Natalia Papina <papina@scamt-itmo.ru>
2024-09-23 22:26:35 +0300

    model has been changed

--- Classifier.py
+++ Classifier.py
@@ -1,22 +1,13 @@
 from train_val_test import x, y_class, split
-from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import BaggingClassifier
 from sklearn.metrics import accuracy_score, roc_auc_score, roc_curve
-from sklearn import set_config
 from visualization import roc_
 
 
-set_config(enable_metadata_routing=True)
-
-
 def model_cla(x, y, name):
-    X_train, X_val, X_test, y_train, y_val, y_test = split(x, y, 0.1, 0.1)
-    MODEL = GaussianNB()
+    X_train, X_val, X_test, y_train, y_val, y_test = split(x, y, 0.2, 0.0001)
+    MODEL = BaggingClassifier()
     MODEL.fit(X_train, y_train)
-    y_val_pred = MODEL.predict(X_val)
-    r2_val = accuracy_score(y_val, y_val_pred)
-    ROC_AUC_val = roc_auc_score(y_val, y_val_pred, average=None)
-    print(f'Accuracy val {name}: {r2_val}')
-    print(f'ROC AUC val {name}: {ROC_AUC_val}')
     y_test_pred = MODEL.predict(X_test)
     r2_test = accuracy_score(y_test, y_test_pred)
     ROC_AUC_test = roc_auc_score(y_test, y_test_pred, average=None)
