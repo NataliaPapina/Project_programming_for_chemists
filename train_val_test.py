55d510aa2f5680b51f8a05ee1e766d541e4e313a
Natalia Papina <papina@scamt-itmo.ru>
2024-09-23 21:05:53 +0300

    _

--- train_val_test.py
+++ train_val_test.py
@@ -5,6 +5,7 @@ x = df_independent_variables
 y_viability = data_normalized['viability']
 y_class = data_normalized['class']
 
+
 def split(x, y, test, val):
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=test, random_state=1)
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=val, random_state=1)
